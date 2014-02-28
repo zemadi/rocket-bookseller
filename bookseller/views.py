5f8f64fb5be49ecd3e1fe6d8666149bec95d17a8
Zhila <zfemadi@gmail.com>
2014-02-27 21:51:36 -0800

    cleaned up forms

--- bookseller/views.py
+++ bookseller/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from bookseller.forms import BookForm, ActorForm
+from bookseller.forms import BookForm, CustomerForm
 from bookseller.models import Book, Customer
 
 
